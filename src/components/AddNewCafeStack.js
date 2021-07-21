61a409b7b4abf0fc76fd3756a1f0cd82c1733110
Bosco Ho <boscojwho@gmail.com>
2021-07-21 16:34:53 -0700

    changed: - replace "arrow" back button with "Cancel" button.

--- src/components/AddNewCafeStack.js
+++ src/components/AddNewCafeStack.js
@@ -2,13 +2,22 @@ import * as React from 'react';
 import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { Button } from 'react-native';
 import AddNewCafeScreen from './AddNewCafeScreen';
 
 const AddNewCafeStack = createStackNavigator();
 const AddNewCafeNavigationStack = () => {
   return (
     <AddNewCafeStack.Navigator initialRouteName="Add New Cafe">
-      <AddNewCafeStack.Screen name="Add New Cafe" component={AddNewCafeScreen} options={{ title: "Add New Cafe" }} />
+      <AddNewCafeStack.Screen 
+        name="Add New Cafe" 
+        component={AddNewCafeScreen} 
+        options={({ navigation }) => ({
+          title: "Add New Cafe",
+          headerLeft: () => (
+            <Button title="Cancel" onPress={ () => navigation.goBack() } />
+          )
+        })} />
     </AddNewCafeStack.Navigator>
   )
 }
