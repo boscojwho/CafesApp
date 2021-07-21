b546b3ea18c77bb6427ea4e6e31f8d1a335a3fc9
Bosco Ho <boscojwho@gmail.com>
2021-07-21 16:30:50 -0700

    added: - simple input form for adding new cafes with basic styling.

--- src/components/AddNewCafeScreen.js
+++ src/components/AddNewCafeScreen.js
@@ -1,11 +1,30 @@
 import * as React from 'react';
 import 'react-native-gesture-handler';
-import { Text } from 'react-native';
+import { Text, TextInput, KeyboardAvoidingView, StyleSheet } from 'react-native';
 
 const AddNewCafeScreen = () => {
   return (
-    <Text>Add New Cafe</Text>
+    <KeyboardAvoidingView behavior={Platform.OS === "ios" ? "padding" : "height"}>
+      <Text style={Styles.textLabel}>Cafe Name</Text>
+      <TextInput style={Styles.textInput} placeholder="e.g. JJ Bean" autoCorrect={false}></TextInput>
+      <Text style={Styles.textLabel}>Cafe Location</Text>
+      <TextInput style={Styles.textInput} placeholder="e.g. East Van" autoCorrect={false}></TextInput>
+    </KeyboardAvoidingView>
   )
 }
+
+const Styles = StyleSheet.create({
+  textInput: {
+    height: 44,
+    margin: 8,
+    borderWidth: 1,
+    padding: 4
+  },
+  textLabel: {
+    marginHorizontal: 8,
+    marginTop: 8,
+    padding: 2
+  }
+})
 
 export default AddNewCafeScreen;
